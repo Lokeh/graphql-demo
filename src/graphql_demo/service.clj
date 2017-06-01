961c7b74736c374200560ea20d61523894cd2460
Will Acton <will@willacton.com>
2017-06-01 12:13:06 -0700

    move service.clj code into server.clj

--- src/graphql_demo/service.clj
+++
@@ -1,9 +0,0 @@
-(ns graphql-demo.service
-  (:require [io.pedestal.http :as http]
-            [io.pedestal.http.route :as route]
-            [io.pedestal.http.body-params :as body-params]
-            [ring.util.response :as ring-resp]
-            [com.walmartlabs.lacinia.pedestal :as lacinia]
-            [graphql-demo.schema :as schema]))
-
-(def service (lacinia/pedestal-service schema/star-wars-schema {:graphiql true}))
